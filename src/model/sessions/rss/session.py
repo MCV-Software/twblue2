d1c071a886a560fce4b77a72699349d44aa59228
Manuel Cortez <manuel@manuelcortez.net>
2022-03-04 20:10:06 -0600

    Added more methods to main Rss session

--- src/model/sessions/rss/session.py
+++ src/model/sessions/rss/session.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """ RSS Session model """
+import feedparser
 from model.sessions import base
 
 class Session(base.Session):
@@ -22,3 +23,18 @@ class Session(base.Session):
     def login(self):
         self.settings["session"]["name"] = self.name
         self.settings.write()
+
+    def get_items(self, site=""):
+        site_rss = self.settings["feeds"].get(site, "")
+        if site_rss == "":
+            raise ValueError("Site name %s does not contain any rss feed" % (site))
+        return site_rss
+
+    def add_site(self, site_name, site_url):
+        content = feedparser.parse(site_url)
+        if len(content.entries) > 0:
+            self.settings["feeds"][site_name] = site_url
+            self.settings.write()
+
+    def get_site(self, site_name):
+        return self.settings.get("feeds", {}).get(site_name, None)
