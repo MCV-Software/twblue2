816a462cdf3b6b92195379623da2d2fe17dea147
Manuel Cortez <manuel@manuelcortez.net>
2022-03-11 12:50:52 -0600

    model.sessions.rss: Added method to order items in a buffer and retrieve items from an RSS feed

--- src/model/sessions/rss/session.py
+++ src/model/sessions/rss/session.py
@@ -2,6 +2,7 @@
 """ RSS Session model """
 import feedparser
 from model.sessions import base
+from typing import List, Dict
 
 class Session(base.Session):
     """ Session model for RSS in TWBlue. 
@@ -24,17 +25,36 @@ class Session(base.Session):
         self.settings["session"]["name"] = self.name
         self.settings.write()
 
+    def order_buffer(self, buffer_name: str, data: List[Dict[str, str]], is_reversed: bool = False) -> int:
+        added_items = 0
+        if self.db.get(buffer_name) == None:
+            self.db[buffer_name] = []
+        objects = self.db[buffer_name]
+        for i in data:
+            # Determine if the object already exists in our local DB.
+            item_found = next((item for item in objects if item["url"] == i["url"]), None)
+            if item_found != None:
+                continue
+            if is_reversed == False:
+                objects.append(i)
+            else:
+                objects.insert(0, i)
+            added_items = added_items + 1
+        self.db[buffer_name] = objects
+        return added_items
+
     def get_items(self, site=""):
         site_rss = self.settings["feeds"].get(site, "")
         if site_rss == "":
             raise ValueError("Site name %s does not contain any rss feed" % (site))
-        return site_rss
+        feed = feedparser.parse(site_rss)
+        results = []
+        for entry in feed.entries:
+            results.append(dict(title=entry.get("title"), url=entry.get("link"), published=entry.get("published"), summary=entry.get("summary")))
+        return results
 
     def add_site(self, site_name, site_url):
         content = feedparser.parse(site_url)
         if len(content.entries) > 0:
             self.settings["feeds"][site_name] = site_url
             self.settings.write()
-
-    def get_site(self, site_name):
-        return self.settings.get("feeds", {}).get(site_name, None)
