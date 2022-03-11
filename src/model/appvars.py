d6d87ee4d0190ba05139ca30fe9709d19d460401
Manuel Cortez <manuel@manuelcortez.net>
2022-03-11 12:52:28 -0600

    model.appvars: Added convenience methods to retrieve sessions

--- src/model/appvars.py
+++ src/model/appvars.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 # Singleton object with all sessions defined in the program.
+
 sessions = {}
+
+class SessionNotFoundException(Exception): pass
+
+def get_session(session_id):
+    global sessions
+    result = sessions.get(session_id)
+    if result == None:
+        raise SessionNotFoundException()
+    return result
+
+def get_sessions():
+    return list(sessions.values())
