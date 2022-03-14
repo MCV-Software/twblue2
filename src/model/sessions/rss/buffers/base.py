a82dd433c2b4fb9006e4b1e90c5a0df437fee06e
Manuel Cortez <manuel@manuelcortez.net>
2022-03-14 14:49:49 -0600

    Reverse list of items before ordering [Skip CI]

--- src/model/sessions/rss/buffers/base.py
+++ src/model/sessions/rss/buffers/base.py
@@ -9,7 +9,10 @@ class RSSBuffer(object):
 
     def get_items(self):
         items = self.session.get_items(*self.args, **self.kwargs)
+        items.reverse()
         new_items = self.session.order_buffer(buffer_name=self.name, data=items)
+        if new_items == 0:
+            return []
         added_items = self.session.db[self.name][-new_items:]
         return added_items
 
