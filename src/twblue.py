23e0503ee8e086a5ed4df54dfa14e0032a5fa728
Manuel Cortez <manuel@manuelcortez.net>
2022-03-14 13:22:42 -0600

    Setup output module on app init

--- src/twblue.py
+++ src/twblue.py
@@ -1,12 +1,13 @@
 ﻿# -*- coding: utf-8 -*-
 import wx # type: ignore
 from controller import mainController, config, sessionManager
-from model import i18n, logger
+from model import i18n, logger, output
 
 app = wx.App()
 logger.setup()
 i18n.setup("TWBlue")
 config.setup()
+output.setup()
 sm = sessionManager.SessionManager()
 sm.start()
 c = mainController.MainController()
