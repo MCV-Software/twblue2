415ddad5911c1f50e809b0beea5543c6605d231a
Manuel Cortez <manuel@manuelcortez.net>
2022-03-14 13:31:45 -0600

    Added methods on rss buffer model to retrieve a single item

--- src/model/sessions/rss/buffers/base.py
+++ src/model/sessions/rss/buffers/base.py
@@ -6,3 +6,12 @@ class RSSBuffer(object):
         self.name = name
         self.args = args
         self.kwargs = kwargs
+
+    def get_items(self):
+        items = self.session.get_items(*self.args, **self.kwargs)
+        new_items = self.session.order_buffer(buffer_name=self.name, data=items)
+        added_items = self.session.db[self.name][-new_items:]
+        return added_items
+
+    def get_item(self, index):
+        return self.session.db[self.name][index]
