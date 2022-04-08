cb6af7269d1c8c92f62b03c5c7435f049de817af
Manuel Cortez <manuel@manuelcortez.net>
2022-04-08 09:50:37 -0500

    Added login method for rss session

--- src/controller/sessions/account.py
+++ src/controller/sessions/account.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from view.sessions import account as view
+from model.sessions import account as model
 from pubsub import pub
 
 class AccountBuffer(object):
@@ -12,6 +13,26 @@ class AccountBuffer(object):
         self.parent = parent
         self.name = buffname
         self.view = None
+        self.model = model.Account(session_id=self.session.session_id)
+        self.subscribe_events()
 
     def create_gui(self):
         self.view = view.Account(parent=self.parent, name=self.session.name)
+
+    def subscribe_events(self):
+        pub.subscribe(self.on_autologin_changed, "account.autologin")
+        pub.subscribe(self.on_login, "account.login")
+
+    def unsubscribe_events(self):
+        pub.unsubscribe(self.on_autologin_changed, "account.autologin")
+        pub.subscribe(self.on_login, "account.login_session")
+
+    def on_autologin_changed(self, name, autologin):
+        if name != self.session.name:
+            return
+        self.model.autologin()
+
+    def on_login(self, name):
+        if self.session.name != name:
+            return
+        self.session.create_buffers(force=True)
--- src/view/sessions/account.py
+++ src/view/sessions/account.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import wx
+from pubsub import pub
 
 class Account(wx.Panel):
     def __init__(self, parent, name=None):
@@ -7,9 +8,11 @@ class Account(wx.Panel):
         self.name = name
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.login = wx.Button(self, wx.ID_ANY, _("Login"))
+        self.login.Bind(wx.EVT_BUTTON, lambda event: pub.sendMessage("account.login", name=name))
         sizer.Add(self.login, 0, wx.ALL, 5)
-        self.autostart = wx.CheckBox(self, wx.ID_ANY, _("Log in automatically"))
-        sizer.Add(self.autostart, 0, wx.ALL, 5)
+        self.autologin = wx.CheckBox(self, wx.ID_ANY, _("Log in automatically"))
+        self.autologin.Bind(wx.EVT_CHECKBOX, lambda event: pub.sendMessage("account.autologin", name=self.name, autologin=self.autologin.GetValue()))
+        sizer.Add(self.autologin, 0, wx.ALL, 5)
         self.SetSizer(sizer)
 
 class EmptyBuffer(wx.Panel):
